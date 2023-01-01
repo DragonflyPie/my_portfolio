60188bdd6838caa47974386ef303e2e52c627c05
dragonflypie <paper-tiger@yandex.ru>
2023-01-01 14:24:47 +0400

    fix: fixed intersection observer logic (outer and inner observers)

--- components/useElementOnScreen.tsx
+++ components/useElementOnScreen.tsx
@@ -1,20 +1,30 @@
 import { RefObject, useEffect, useState } from "react";
 
 function useElementOnScreen(ref: RefObject<Element>, rootMargin = "0px") {
-  const [isIntersecting, setIsIntersecting] = useState(true);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
   useEffect(() => {
-    const observer = new IntersectionObserver(
+    const outerObsersver = new IntersectionObserver(
       ([entry]) => {
-        setIsIntersecting(entry.isIntersecting);
+        if (!entry.isIntersecting) setIsIntersecting(false);
       },
       { rootMargin }
     );
+    const innerObserver = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) setIsIntersecting(true);
+      },
+      { rootMargin: "-300px 0px" }
+    );
+
     if (ref.current) {
-      observer.observe(ref.current);
+      outerObsersver.observe(ref.current);
+      innerObserver.observe(ref.current);
     }
     return () => {
       if (ref.current) {
-        observer.unobserve(ref.current);
+        innerObserver.unobserve(ref.current);
+        outerObsersver.unobserve(ref.current);
       }
     };
   }, []);
